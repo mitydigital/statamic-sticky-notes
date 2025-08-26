3c3ec9e64db5f3eaaab6f4c61d72225462266946
Marty Friedel <marty.dev@mity.com.au>
2025-08-26 14:08:17 +0930

    Tailwind 4 setup

--- resources/views/widgets/sticky-notes.blade.php
+++ resources/views/widgets/sticky-notes.blade.php
@@ -8,7 +8,7 @@
                 </div>
             </header>
             @if ($intro)
-                <div class="ml-8">
+                <div class="ml-8 text-xs text-gray-500">
                     <p>{{ $intro }}</p>
                 </div>
             @endif
@@ -18,32 +18,4 @@
             @include('statamic-sticky-notes::widgets._content', ['content' => $content])
         </div>
     </ui-card>
-
-    <div id="statamic-sticky-notes-widget" class="card p-0 overflow-hidden h-full">
-        <div class="border-b dark:border-dark-900">
-            <div class="flex justify-between items-center p-4">
-                <h2 class="flex">
-                    <div class="h-6 w-6 mr-3 text-gray-800 dark:text-dark-175">
-                        {!! $icon !!}
-                    </div>
-                    <div class="-mt-px">
-                        <div>{{ $heading }}</div>
-
-                        @if ($intro)
-                            <div class="content">
-                                <p>{{ $intro }}</p>
-                            </div>
-                        @endif
-                    </div>
-                </h2>
-            </div>
-        </div>
-
-        <div>
-            <div class="p-4">
-                @include('statamic-sticky-notes::widgets._content', ['content' => $content])
-            </div>
-        </div>
-
-    </div>
 @endif
